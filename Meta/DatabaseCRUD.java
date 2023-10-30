41b7623f8f08b1ad8b1840da363f46c58d66d94e
thomas.vandecasteele <thomas.vandecasteele@le-campus-numerique.fr>
2023-10-30 13:04:20 +0100

    jpp

--- Meta/DatabaseCRUD.java
+++ Meta/DatabaseCRUD.java
@@ -124,12 +124,11 @@ public class DatabaseCRUD {
 
     /*---------------------------Méthodes pour le plateau---------------------------*/
     public int createPlateau(Characters player)throws SQLException{
-        PreparedStatement pstmt= mydb.prepareStatement("INSERT INTO plateau (gobelin,sorcier,dragon,massue,epee,eclair,boule_de_feu,potion_m,potion_g,vide) VALUES (?,?,?,?,?,?,?,?,?,?) UNION SELECT id FROM plateau WHERE vide=?",Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement pstmt= mydb.prepareStatement("INSERT INTO plateau (gobelin,sorcier,dragon,massue,epee,eclair,boule_de_feu,potion_m,potion_g,vide) VALUES (?,?,?,?,?,?,?,?,?,?)",Statement.RETURN_GENERATED_KEYS);
         String[] tileList=getTileList(player.getPlateau());
         for (int i = 0; i < 10; i++) {
             pstmt.setString(i+1,tileList[i]);
         }
-        pstmt.setString(10,tileList[9]);
         pstmt.executeUpdate();
         ResultSet rs= pstmt.getGeneratedKeys();
         int id=0;
@@ -158,7 +157,7 @@ public class DatabaseCRUD {
         return getArrayList(list);
     }
     public void updatePlateau(Characters player)throws SQLException{
-        PreparedStatement pstmt= mydb.prepareStatement("UPDATE INTO plateau SET gobelin=?,sorcier=?,dragon=?,massue=?,epee=?,eclair=?,boule_de_feu=?,potion_m=?,potion_g=?,vide=? WHERE plateau_id=?");
+        PreparedStatement pstmt= mydb.prepareStatement("UPDATE plateau SET gobelin=?,sorcier=?,dragon=?,massue=?,epee=?,eclair=?,boule_de_feu=?,potion_m=?,potion_g=?,vide=? WHERE plateau_id=?");
         String[] tileList=getTileList(player.getPlateau());
         for (int i = 0; i < 10; i++) {
             pstmt.setString(i+1,tileList[i]);
@@ -184,35 +183,45 @@ public class DatabaseCRUD {
         int[] potionsG = toIntArray(enemyList[8].split(" "));
         int[] vides = toIntArray(enemyList[9].split(" "));
         for (int i = 0; i < 64; i++) {
-            if (gobelins[0]==i){
+            if (gobelins.length>0&&gobelins[0]==i){
                 al.add(new Enemy("Gobelin", 6, 1, i));
-            }else if (sorciers[0]==i){
+                gobelins=Arrays.copyOfRange(gobelins,1,gobelins.length);
+            }else if (sorciers.length>0&&sorciers[0]==i){
                 al.add(new Enemy("Sorcier", 9, 2, i));
-            }else if (dragons[0]==i){
+                sorciers=Arrays.copyOfRange(sorciers,1,sorciers.length);
+            }else if (dragons.length>0&&dragons[0]==i){
                 al.add(new Enemy("Dragon", 15, 4, i));
-            }else if (massues[0]==i){
+                dragons=Arrays.copyOfRange(dragons,1,dragons.length);
+            }else if (massues.length>0&&massues[0]==i){
                 al.add(new Massue(i));
-            }else if (epees[0]==i){
+                massues=Arrays.copyOfRange(massues,1,massues.length);
+            }else if (epees.length>0&&epees[0]==i){
                 al.add(new Epee(i));
-            }else if (eclairs[0]==i){
+                epees=Arrays.copyOfRange(epees,1,epees.length);
+            }else if (eclairs.length>0&&eclairs[0]==i){
                 al.add(new Eclair(i));
-            }else if (boules[0]==i){
+                eclairs=Arrays.copyOfRange(eclairs,1,eclairs.length);
+            }else if (boules.length>0&&boules[0]==i){
                 al.add(new BouleDeFeu(i));
-            }else if (potionsM[0]==i){
+                boules=Arrays.copyOfRange(boules,1,boules.length);
+            }else if (potionsM.length>0&&potionsM[0]==i){
                 al.add(new PotionM(i));
-            }else if (potionsG[0]==i){
+                potionsM=Arrays.copyOfRange(potionsM,1,potionsM.length);
+            }else if (potionsG.length>0&&potionsG[0]==i){
                 al.add(new PotionG(i));
-            }else if (vides[0]==i) {
+                potionsG=Arrays.copyOfRange(potionsG,1,potionsG.length);
+            }else if (vides.length>0&&vides[0]==i) {
                 al.add(new EmptyTile(i));
+                vides=Arrays.copyOfRange(vides,1,vides.length);
             }
         }
         return al;
     }
 
     public int[] toIntArray(String[] s){
-        int[] result = new int[s.length];
+        int[] result = new int[s.length-1];
         for (int i = 1; i < s.length; i++) {
-            result[i] = Integer.parseInt(s[i]);
+            result[i-1] = Integer.parseInt(s[i]);
         }
         return result;
     }
